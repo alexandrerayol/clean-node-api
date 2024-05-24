745617582e92f4983d006f741324081e56baa181
Alexandre Rayol <alexandrerayol.dev@gmail.com>
2024-05-24 19:17:50 -0300

    chore: add jest

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,6 +1,17 @@
+const { rules } = require('eslint-config-love');
+
 module.exports = [
     {
-        ...require('eslint-config-love'),
-        files: ['*.js', '*.jsx', '*.ts', '*.tsx'],
-    }
-]
+      ...require('eslint-config-love'),
+      files: [
+        'src/**/*.js',
+        'src/**/*.jsx',
+        'src/**/*.ts',
+        'src/**/*.tsx'
+      ],
+      rules: {
+        ...rules,
+        "@typescript-eslint/strict-boolean-expressions": "warn"
+      }
+    },
+  ]
