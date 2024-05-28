b13eb9725cbd982335723eae7188b23ae83bfb4d
Alexandre Rayol <alexandrerayol.dev@gmail.com>
2024-05-28 11:04:09 -0300

    test: ensure SignUpController return an error if no name is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,6 +1,6 @@
 import { SignUpController } from './signup'
 
-describe('Signup Controller', () => {
+describe('SignUp Controller', () => {
   test('Should return 400 if no name is provided', () => {
     const sut = new SignUpController()
     const httpRequest = {
@@ -12,5 +12,6 @@ describe('Signup Controller', () => {
     }
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: name'))
   })
 })
